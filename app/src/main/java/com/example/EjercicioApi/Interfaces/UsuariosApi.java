d03a8210294446ccda402adb5ac7d0c7e9c6ddba
ajocasar <99230237+ajocasar@users.noreply.github.com>
2022-07-11 21:42:08 -0600

    Ejercicio 2.2

---
+++ app/src/main/java/com/example/EjercicioApi/Interfaces/UsuariosApi.java
@@ -0,0 +1,21 @@
+package com.example.EjercicioApi.Interfaces;
+
+import com.example.EjercicioApi.Models.Usuarios;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface UsuariosApi
+{
+    String Ruta0 = "/posts";
+    String Ruta1= "/posts/{valor}";
+
+    @GET(Ruta0)
+    Call<List<Usuarios>> getUsuarios();
+
+    @GET(Ruta1)
+    Call<Usuarios> getUsuario(@Path("valor") String valor);
+}
